784719307d2d2e185b784531bb0e45b809405294
Felipe Bezerra <felipe_angells@hotmail.com>
2014-09-13 14:17:14 -0300

    new folders

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,5 @@
-var express = require('express');
-var router = express.Router();
-
 module.exports = function(app) {
+	
 	var home = app.controllers.home;
 	app.get('/', home.index);
-}
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+};
